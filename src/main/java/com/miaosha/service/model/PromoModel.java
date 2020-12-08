5a2e01bf2093c14bcb0a433c295fa0261b475710
Weisun <1210029395@qq.com>
2020-12-08 16:48:26 +0800

    秒杀模块开发

--- src/main/java/com/miaosha/service/model/PromoModel.java
+++ src/main/java/com/miaosha/service/model/PromoModel.java
@@ -1,4 +1,27 @@
 package com.miaosha.service.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
 public class PromoModel {
+    private Integer id;
+
+    //秒杀活动名称
+    private String promoName;
+
+    //秒杀活动开始时间
+    private DateTime startDate;
+
+    //秒杀活动适用的商品
+    private Integer itemId;
+
+    //秒杀活动的商品价格
+    private BigDecimal promoItemPrice;
+
 }

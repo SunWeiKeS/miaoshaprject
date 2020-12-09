ceb85892da2b4a09be9f86711642c13ee4f11d8f
Weisun <1210029395@qq.com>
2020-12-09 11:10:03 +0800

    完成

--- src/main/java/com/miaosha/controller/OrderController.java
+++ src/main/java/com/miaosha/controller/OrderController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller("order")
 @RequestMapping("/order")
 @CrossOrigin(origins = {"*"}, allowCredentials = "true")
-public class OrderController extends BaseController{
+public class OrderController extends BaseController {
     @Autowired
     private OrderService orderService;
 
@@ -25,16 +25,18 @@ public class OrderController extends BaseController{
     //封装下单请求
     @RequestMapping(value = "/createorder", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
     @ResponseBody
-    public CommonReturnType createOrder(@RequestParam(name = "itemId")Integer itemId,
-                                        @RequestParam(name = "amount")Integer amount) throws BusinessException {
-        Boolean isLogin =(Boolean)httpServletRequest.getSession().getAttribute("IS_LOGIN");
-        if(isLogin==null||!isLogin.booleanValue()){
-            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN,"用户还未登录，不能下单");
+    public CommonReturnType createOrder(@RequestParam(name = "itemId") Integer itemId,
+                                        @RequestParam(name = "amount") Integer amount,
+                                        @RequestParam(name = "promoId", required = false) Integer promoId)
+            throws BusinessException {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if (isLogin == null || !isLogin.booleanValue()) {
+            throw new BusinessException(EmBusinessError.USER_NOT_LOGIN, "用户还未登录，不能下单");
         }
 
         //获取用户登录信息
-        UserModel userModel = (UserModel)httpServletRequest.getSession().getAttribute("LOGIN_USER");
-        orderService.createOrder(userModel.getId(), itemId, amount);
+        UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
+        orderService.createOrder(userModel.getId(), itemId, promoId, amount);
         return CommonReturnType.create(null);
     }
 
